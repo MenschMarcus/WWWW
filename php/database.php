16b1f63323a1d85c675275613b6bb8662a5c932e
KlangSinn <Sascha.Dobschal@histoglobe.com>
2014-06-16 15:13:02 +0200

    database stuff

--- php/database.php
+++ php/database.php
@@ -5,16 +5,16 @@ class database {
   private $connection;
 
   function __construct() {
-    $mysqli = new mysqli('lvps5-35-242-120.dedicated.hosteurope.de:3306', 'wwww', '1q2w3e4r.', 'wwww');
-    // $link = mysql_connect('histoglobe.com:3306', 'wwww', '1q2w3e4r.');
-    // if(mysql_select_db ("wwww", $link)) {
-    //   $this->connection = $link;
-    // } else {
-    //   $this->connection = false;
-    // }
-    // if (!$link) {
-    //   die('Verbindung zur Datenbank schlug fehl: ' . mysql_error());
-    // }
+    //$mysqli = new mysqli('lvps5-35-242-120.dedicated.hosteurope.de:3306', 'wwww', '1q2w3e4r.', 'wwww');
+     $link = mysql_connect('lvps5-35-242-120.dedicated.hosteurope.de:3306', 'wwww', '1q2w3e4r.');
+     if(mysql_select_db ("wwww", $link)) {
+       $this->connection = $link;
+     } else {
+       $this->connection = false;
+     }
+    if (!$link) {
+     die('Verbindung zur Datenbank schlug fehl: ' . mysql_error());
+    }
   }
 
   public function close() {
