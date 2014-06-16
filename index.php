ed49912e2d7a7687f04992331fcfa7b977ee6637
Simon Schneegans <code@simonschneegans.de>
2014-06-16 15:15:59 +0200

    added bootstrap

--- index.php
+++ index.php
@@ -4,11 +4,27 @@
 
 <head>
     <title>WWWW</title>
+    <script type="text/javascript" src="script/jquery-1.9.0.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="style/bootstrap-theme.css">
+    <link rel="stylesheet" type="text/css" href="style/style.css" />
 </head>
 <body>
-    <div id="start">
-        <a href="question.php">STARTEN!</a>
+    <div class="phone-outer">
+        <div class="phone-inner">
+            <div class="start">
+                <a href="question.php">STARTEN!</a>
+            </div>
+            <div class="question-bar"></div>
+            <div class="control-bar">
+                <div class="btn btn-danger btn-lg"><span class="glyphicon glyphicon-remove"></span></div>
+                <div class="btn btn-success btn-lg pull-right"><span class="glyphicon glyphicon-ok"></span></div>
+
+            </div>
+        </div>
     </div>
+
+    <script src="js/bootstrap.min.js"></script>
 </body>
 
 </html>
