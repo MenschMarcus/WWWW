68cf9f98008e4d4cefb405f8fcf60e6557bc8053
Felix Lauer <felix.lauer@uni-weimar.de>
2014-06-24 13:41:07 +0200

    added browser detection; mobile devices are not allowed to play

--- index.php
+++ index.php
@@ -3,25 +3,48 @@
 <html lang="de">
 
 <head>
-    <title>WWWW</title>
-    <script type="text/javascript" src="script/third-party/jquery-1.10.2.js"></script>
-    <link rel="stylesheet" type="text/css" href="style/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="style/bootstrap-theme.css">
-    <link rel="stylesheet" type="text/css" href="style/style.css" />
+  <meta charset="utf-8">
+  <title>WWWW</title>
+  <script type="text/javascript" src="script/third-party/jquery-1.10.2.js"></script>
+  <script type="text/javascript" src="script/third-party/BrowserDetect.js"></script>
+
+  <script type="text/javascript" src="build/BrowserDetector.js"></script>
+
+  <link rel="stylesheet" type="text/css" href="style/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="style/bootstrap-theme.css">
+  <link rel="stylesheet" type="text/css" href="style/style.css" />
+  <link rel="stylesheet" type="text/css" href="style/feedback.css" />
+
+  <script type="text/javascript">
+    $(document).ready(function($) {
+      $("#feedback-fail").hide()
+      var browserDetector = new WWWW.BrowserDetector();
+      var p = browserDetector.platform;
+
+      if (p=="Android" || p=="iPhone" || p=="iPad" || p=="unknown") {
+        $("#start-button").hide()
+        $("#instructions").hide()
+        $("#feedback-fail").hide().slideDown()
+      }
+    });
+  </script>
 </head>
 <body>
-    <div class="phone-outer">
-        <div class="phone-inner">
-            <h1 class="text-center">Was War Wann Wo?</h1>
-            <p class="text-center" style="margin-top:200px"><img src="img/marker_map.png" /></p>
-            <h3 class="text-center">Platziere beide Marker an die richtigen Stellen auf der Zeitleiste und der Karte!</h3>
-            <div class="well start">
-                <a href="question.php" type="button" class="btn btn-success btn-lg btn-block">Spiel starten!</a>
-            </div>
-        </div>
+  <div class="phone-outer">
+    <div class="phone-inner">
+      <h1 class="text-center">Was War Wann Wo?</h1>
+      <div id="instructions">
+        <p class="text-center" style="margin-top:200px"><img src="img/marker_map.png" /></p>
+        <h3 class="text-center">Platziere beide Marker an die richtigen Stellen auf der Zeitleiste und der Karte!</h3>
+      </div>
+      <div id="feedback-fail" class="feedback-start-fail"> WWWW läuft leider noch nicht auf deiner Plattform! Probiere es an deinem Rechner.</div>
+      <div id="start-button" class="well start">
+        <a href="question.php" type="button" class="btn btn-success btn-lg btn-block">Spiel starten!</a>
+      </div>
     </div>
+  </div>
 
-    <script src="js/bootstrap.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
 </body>
 
 </html>
