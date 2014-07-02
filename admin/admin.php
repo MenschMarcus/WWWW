24de697b24c52a79396a132009d89d1d9088b9b8
Felix Lauer <felix.lauer@uni-weimar.de>
2014-07-02 17:58:29 +0200

    prepared admin.php for content contest

--- admin/admin.php
+++ admin/admin.php
@@ -13,9 +13,9 @@
   <script type="text/javascript" src="../build/executePHPFunction.js"></script>
 
 
-  <link rel="stylesheet" type="text/css" href="../style/bootstrap.css">
-  <link rel="stylesheet" type="text/css" href="../style/bootstrap-theme.css">
-  <link rel="stylesheet" type="text/css" href="../style/bootstrap-social.css">
+  <link rel="stylesheet" type="text/css" href="../style/third-party/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="../style/third-party/bootstrap-theme.css">
+  <link rel="stylesheet" type="text/css" href="../style/third-party/bootstrap-social.css">
   <link rel="stylesheet" type="text/css" href="../style/admin.css" />
   <link rel="stylesheet" type="text/css" href="../style/style.css" />
   <link rel="stylesheet" type="text/css" href="../style/marker.css" />
@@ -52,10 +52,10 @@
         var marker = new google.maps.Marker({
           map: map,
           position: new google.maps.LatLng(lat, lng)
-        });      
+        });
     }
 
-    function findLatLng() {      
+    function findLatLng() {
       geocoder = new google.maps.Geocoder();
       InitializeMap();
 
@@ -68,7 +68,7 @@
                   position: results[0].geometry.location
               });
               document.getElementById("lat").value = results[0].geometry.location.k;
-              document.getElementById("lng").value = results[0].geometry.location.A;
+              document.getElementById("lng").value = results[0].geometry.location.B;
           }
           else {
               alert("Ort konnte leider nicht gefunden werden. \n\nError: " + status);
@@ -90,24 +90,22 @@
       $("#sure-send").click(
         function() {
           var send = {
-            table: "question",
+            table: "question_content_contest",
             values: "'"+fields[0].val()+"',"+
-                    "'"+fields[7].val()+"',"+
-                    "'"+fields[6].val()+"',"+
                     "'"+fields[3].val()+"',"+
                     "'"+fields[1].val()+"',"+
                     "'"+fields[2].val()+"',"+
                     "'"+fields[4].val()+"',"+
-                    "'"+(fields[8].prop("checked") ? 1 : 0) +"',"+
-                    "'"+fields[5].val()+"'",
-            names: "`text`, `map_id`, `tl_id`, `location`, `lat`, `long`, `year`, `funny`, `answer`"
+                    "'"+fields[5].val()+"',"+
+                    "'"+fields[6].val()+"'",
+            names: "`text`, `location`, `lat`, `long`, `year`, `answer`, `group_name`"
           };
 
           WWWW.executePHPFunction( "insertIntoDB", send,
             function(response) {
               console.log("question submitted with response " + response);
               var len = fields.length;
-              for (var i=0; i<len; i++) {
+              for (var i=0; i<len-1; i++) { // IGNORES GROUP NAME FIELD!
                 fields[i].val("");
               }
             },
@@ -125,9 +123,7 @@
           fields[3] = $('input[name=location]');
           fields[4] = $('input[name=year]');
           fields[5] = $('textarea[name=info]');
-          fields[6] = $('input[name=timeline]');
-          fields[7] = $('input[name=map]');
-          fields[8] = $('input[name=funny]');
+          fields[6] = $('input[name=group-name]');
 
           var allFieldsSet = true;
           var len = fields.length;
@@ -162,6 +158,12 @@
 
 <body>
   <div class="stat-group">
+    <br/>
+    <div id="location-group" class="form-group col-xs-12 floating-label-form-group">
+      <label class="control-label" for="location">Gruppenname</label>
+      <input id="location" class="form-control" type="text" name="group-name" placeholder="Name eurer Gruppe">
+    </div>
+    <br/>
     <div id="question-group" class="form-group col-xs-12 floating-label-form-group">
       <label class="control-label" for="question">Fragetext</label>
       <textarea name="question" placeholder="Fragetext" class="form-control" rows="5"></textarea>
@@ -193,7 +195,7 @@
           <div id="map"></div>
         </td>
       </tr>
-    </table>    
+    </table>
     <br/>
     <div id="year-group" class="form-group col-xs-12 floating-label-form-group">
       <label class="control-label" for="year">Antwort Jahr</label>
@@ -203,23 +205,7 @@
       <label class="control-label" for="info">Infotext</label>
       <textarea id="info" name="info" placeholder="Infotext" class="form-control" rows="5"></textarea>
     </div>
-    <br/>
-    <div id="map-group" class="form-group col-xs-12 floating-label-form-group">
-      <label class="control-label" for="map">Karten ID</label>
-      <input id="map" class="form-control" type="text" name="map" placeholder="Karten ID">
-    </div>
-    <br/>
-    <div id="timeline-group" class="form-group col-xs-12 floating-label-form-group">
-      <label class="control-label" for="timeline">Zeitleisten ID</label>
-      <input id="timeline" class="form-control" type="text" name="timeline" placeholder="Zeitleisten ID">
-    </div>
-    <br/>
-    <div id="funny-group" class="form-group col-xs-12 floating-label-form-group">
 
-      <label for="funny">
-        <input id="funny" type="checkbox" name="funny"> Kuriose Frage
-      </label>
-    </div>
     <br/>
 
     <div id="submit" class="btn btn-success">Abschicken!</div>
@@ -232,17 +218,13 @@
           <h4 class="modal-title">Wirklich abschicken?</h4>
         </div>
         <div class="modal-body">
-          Denk dran: Für eine Korrektur musst du dich erst auf dem Server einloggen und 1000000 Mal klicken, bevor du an der richten Stelle bist!
+          <!-- Denk dran: Für eine Korrektur musst du dich erst auf dem Server einloggen und 1000000 Mal klicken, bevor du an der richten Stelle bist! -->
           <br/>
-          Also hast du...
+          Hast du...
           <br/>
           ... die Rechtschreibung geprüft?
           <br/>
           ... die korrekten Koordinaten und Vorzeichen bei Latitude und Longitude eingetragen?
-          <br/>
-          ... die richtigen IDs für Karte und Zeitleiste angegeben?
-          <br/>
-          ... angehakt, ob die Frage kurios ist oder nicht?
 
         </div>
         <div class="modal-footer">
@@ -253,7 +235,7 @@
     </div>
   </div>
 
-   <script src="../js/bootstrap.min.js"></script>
+   <script src="../script/third-party/bootstrap.min.js"></script>
 </body>
 
 </html>
