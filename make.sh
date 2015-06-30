6767ce5e991930de17d5529681c02b9bf23e4606
Simon Schneegans <code@simonschneegans.de>
2015-06-30 21:57:19 +0200

    added automatic version

--- make.sh
+++ make.sh
@@ -7,6 +7,12 @@ coffee --compile --output $DIR/build/ $DIR/coffee/
 
 jFiles=$(find build -name '*.js')
 
+VERMAJOR=0
+VERMINOR=1
+VERSION=`date +%Y.%m.%d.%H.%M`
+
+echo "@version: 'built $VERMAJOR.$VERMINOR.$VERSION';" > "$DIR/less/version.less"
+
 uglifyjs $jFiles -o $DIR/www/js/wwww.min.js -mc
 
 lessc --no-color -x less/wwww.less www/css/wwww.min.css
